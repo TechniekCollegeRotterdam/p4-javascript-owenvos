897d184297f854b714d87f8c75f3092d30170bf1
owenvos <9016029@student.zadkine.nl>
2022-05-17 12:40:10 +0200

    oefening 1 en 2

---
+++ app.js
@@ -0,0 +1,30 @@
+let name;
+let country;
+let p = document.getElementById("output");
+let nameChange = document.getElementById("nameChange");
+let countryChange = document.getElementById("countryChange");
+
+
+nameChange.addEventListener("change", function(){
+    name = event.target.value;
+changeInput();
+});
+countryChange.addEventListener("change", function(){
+    country = event.target.value;
+    changeInput();
+}
+);
+// const nameChange = function(event){
+//      name = event.target.value;
+//      changeInput();
+// };
+
+// const countryChange = function(event){
+//    country = event.target.value;
+//    changeInput();
+// };
+
+const changeInput = function(){
+    const text = `Je heet ${name} en je komt uit ${country}`; 
+    p.textContent = text;
+};
