c02d73afe4e3d1b0e0dc474c8d2bfccee162cf98
owenvos <9016029@student.zadkine.nl>
2022-05-31 12:25:02 +0200

    oefening 3

--- les-2/angry-birds/app.js
+++ les-2/angry-birds/app.js
@@ -12,18 +12,22 @@ window.addEventListener("keydown", function(event){
     if(event.key == "ArrowRight"){
         count = count + 50;
         bird.style.left = count + "px" ;
+        bird.src = "img/b-right.svg";
     };
     if(event.key == "ArrowLeft"){
         count = count - 50;
         bird.style.left = count + "px" ;
+        bird.src = 'img/b-left.svg'
     };
     if(event.key == "ArrowUp"){
         count = count + 50;
         bird.style.bottom = count + "px" ;
+        bird.src = "img/b-down.svg"
     };
     if(event.key == "ArrowDown"){
         count = count - 50;
         bird.style.bottom = count + "px" ;
+        bird.src = "img/b-up.svg"
     };
 });
 
