0d9455536891cf5b9edd1b20075085df7bfc9f1f
owenvos <9016029@student.zadkine.nl>
2022-06-09 08:37:05 +0200

    start player

--- game/index.js
+++ game/index.js
@@ -1,4 +1,25 @@
 //ik haal hier canvas op en sla het op in een variable
-const canvas = document.querySelector('canvas');
+const canvas = document.querySelector('canvas')
 
-const c = canvas.getContext('2d');
+const c = canvas.getContext('2d')
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+class player{
+constructor(){
+    this.position = {
+        x: 100,
+        y: 100
+    }
+    this.width = 100
+    this.height = 100
+}
+draw(){
+    c.fillRect(this.position.x, this.position.y, this.width, this.height)
+}
+
+}
+
+const player = new player()
+    player.draw()
