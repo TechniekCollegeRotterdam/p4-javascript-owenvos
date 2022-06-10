ffd478d6672d91fc3bc3270d002d6002282f3a3c
owenvos <9016029@student.zadkine.nl>
2022-06-10 14:12:02 +0200

    movement part 1

--- game/index.js
+++ game/index.js
@@ -6,7 +6,7 @@ const c = canvas.getContext('2d')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-const gravity= 1.5
+const gravity = 1.5
 class Player {
     constructor() {
         this.position = {
@@ -25,22 +25,50 @@ class Player {
         c.fillStyle = 'red'
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
-update(){
-    this.draw()
-    this.position.y += this.velocity.y
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
 
-    if(this.position.y + this.height +  this.velocity.y <= canvas.height)
-    this.velocity.y += gravity
-    else this.velocity.y = 0
-}
+        if (this.position.y + this.height + this.velocity.y <= canvas.height)
+            this.velocity.y += gravity
+        else this.velocity.y = 0
+    }
 }
 
 const player = new Player()
 player.update()
 
-function animate(){
+function animate() {
     requestAnimationFrame(animate)
-    c.clearRect(0,0, canvas.width, canvas.height)
+    c.clearRect(0, 0, canvas.width, canvas.height)
     player.update()
 }
 animate()
+
+addEventListener('keydown', ({
+    keyCode
+}) => {
+    // console.log(keyCode)
+    switch (keyCode) {
+        case 65:
+            console.log('left')
+
+            break
+
+        case 83:
+            console.log('down')
+
+            break
+
+        case 68:
+            console.log('right')
+            player.velocity.x += 1
+            break
+
+        case 87:
+            console.log('up')
+            player.velocity.y -= 20
+            break
+    }
+})
