d17413eade89d6b984c3fed721ad769081d8bf95
owenvos <9016029@student.zadkine.nl>
2022-06-15 16:12:14 +0200

    platform update

--- game/index.js
+++ game/index.js
@@ -37,15 +37,15 @@ class Player {
 }
 
 class Platform {
-    constructor(){
+    constructor() {
         this.position = {
             x: 200,
             y: 100
         }
         this.width = 200
-        this.height= 20
+        this.height = 20
     }
-    draw(){
+    draw() {
         c.fillStyle = 'blue'
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
@@ -73,7 +73,11 @@ function animate() {
         player.velocity.x = 5
     } else if (keys.left.pressed) {
         player.velocity.x = -5
-    }else player.velocity.x = 0
+    } else player.velocity.x = 0
+    // platform detection
+    if (player.position.y + player.height <= platform.position.y && player.position.y + player.height + player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width) {
+        player.velocity.y = 0
+    }
 }
 animate()
 
